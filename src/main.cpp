bcc78155c8c36c3a7cdb07ef778899b917b0add4
wyl8899 <wyl8899k@gmail.com>
2015-06-29 09:50:40 +0800

    #includes now moved to src/CommonIncludes.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,5 @@
+#include "CommonIncludes.h"
 #include "CustomSolver.h"
-
-#include <ale_interface.hpp>
-
-#include <glog/logging.h>
-
-#include <cstring>
-#include <map>
-#include <string>
-#include <vector>
-
-#include "boost/algorithm/string.hpp"
-#include "caffe/caffe.hpp"
-
-
-using caffe::Blob;
-using caffe::Caffe;
-using caffe::Net;
-using caffe::Layer;
-using caffe::shared_ptr;
-using caffe::Timer;
-using caffe::vector;
-
-using caffe::SGDSolver;
-using caffe::SolverParameter;
-using caffe::caffe_set;
-
-// =====================================================================
-
-DEFINE_int32(gpu, -1,
-    "Run in GPU mode on given device ID.");
-DEFINE_string(solver, "",
-    "The solver definition protocol buffer text file.");
-DEFINE_string(model, "",
-    "The model definition protocol buffer text file..");
-DEFINE_string(snapshot, "",
-    "Optional; the snapshot solver state to resume training.");
-DEFINE_string(weights, "",
-    "Optional; the pretrained weights to initialize finetuning. "
-    "Cannot be set simultaneously with snapshot.");
-DEFINE_int32(iterations, 50,
-    "The number of iterations to run.");
-
-// Load the weights from the specified caffemodel(s) into the train and
-// test nets.
-void CopyLayers(caffe::Solver<float>* solver, const std::string& model_list) {
-  std::vector<std::string> model_names;
-  boost::split(model_names, model_list, boost::is_any_of(",") );
-  for (int i = 0; i < model_names.size(); ++i) {
-    LOG(INFO) << "Finetuning from " << model_names[i];
-    solver->net()->CopyTrainedLayersFrom(model_names[i]);
-    for (int j = 0; j < solver->test_nets().size(); ++j) {
-      solver->test_nets()[j]->CopyTrainedLayersFrom(model_names[i]);
-    }
-  }
-}
-
-// =========================================================================
 
 int main(int argc, char** argv) {
   // Print output to stderr (while still logging).
@@ -104,8 +48,6 @@ int main(int argc, char** argv) {
   LOG(INFO) << "Optimization Done.";
   return 0;
 }
-
-// ==================================================================
 
 #ifdef __USE_SDL
   #include <SDL.h>
